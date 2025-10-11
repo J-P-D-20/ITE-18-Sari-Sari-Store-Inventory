4f38fba2ed32844964e33cd100c2f69217415685
adlawon-e <aestrellemae@gmail.com>
2025-10-11 16:56:41 +0800

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,26 @@
 [
   {
+    "id": 1,
+    "ProductName": "Rice",
+    "Price": 50,
+    "Quantity": 15
+  },
+  {
     "id": 2,
-    "ProductName": "coke",
+    "ProductName": "Coke",
     "Price": 30,
     "Quantity": 40
   },
   {
-    "ProductName": "keyboard",
-    "Price": 30,
+    "id": 3,
+    "ProductName": "Keyboard",
+    "Price": 300,
     "Quantity": 5
+  },
+  {
+    "id": 4,
+    "ProductName": "Milk",
+    "Price": 10,
+    "Quantity": 0
   }
 ]
